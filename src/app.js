8c9312e81746a3e989be1a33a905a4e5b5b0993e
Jose Manuel <joseman6575@gmail.com>
2024-01-26 15:25:19 -0500

    start

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  var domainName = document.querySelector("#domainName");
+  console.log(domainName);
+  let pronoun = ["the", "our", "my", "your", "his", "her"];
+  let adj = ["great", "big", "small", "tiny", "cute", "ugly"];
+  let noun = ["jogger", "racoon", "dog", "cat", "fish", "yacom"];
+  let domain = [".com", ".net", ".us", ".io", ".es", ".co"];
+  pronoun.forEach((pronounItem) =>{
+    adj.forEach((adjItem) => {
+      noun.forEach((nounItem) => {
+        domain.forEach((domainItem) => {
+          domainName.innerHTML += "<li>"+pronounItem+adjItem+nounItem+domainItem +"</li>";
+        });
+      });
+    });
+  });
+}
+
